a5f5b1facc9b60c64d306bfef397438c07dc73da
이민석 <115360167+leeminseok07@users.noreply.github.com>
2023-11-03 17:46:49 +0900

    Update Config.ts

--- Bakery/src/Config/Config.ts
+++ Bakery/src/Config/Config.ts
@@ -1,1 +1,1 @@
-export const TOKEN: string = process.env.TOKEN || "MTE1MDcwNTI0NzcwNzIwMTU1Nw.GCNK4N.ypwLpRxMkPuh6GC9WrAD6IMQbdyqv-TSgz5eMI";
+export const TOKEN: string = process.env.TOKEN || "비밀";
